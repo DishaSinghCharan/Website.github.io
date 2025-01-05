860215cae6dc7027ae6ab12558e0dca1729bcc92
siddesh <64972996+siddeshrohri@users.noreply.github.com>
2025-01-05 23:16:01 +0000

    UI

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export", // Enable static export mode
+  basePath: "/Website.github.io", // Replace with your GitHub repo name
   // distDir: 'dist',  // Optional: You can specify a custom output directory
 };
 
